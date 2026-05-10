7aef5f5df216057e592f4ad7cdaab8681d5a979a
AshwiniMastani <ashwinimastani@gmail.com>
2026-05-10 12:20:15 +0530

    Add files via upload

---
+++ ASHWINI MASTANI PROGRAM 10.py
@@ -0,0 +1,4 @@
+a="hello everyone"
+b="good morning"
+print(a.replace("e","a"))
+print(b.replace("o","ooo")) #REPLACING STRINGS
---
+++ ASHWINI MASTANI PROGRAM 9.py
@@ -0,0 +1,18 @@
+x="Good Morning"
+print(x[2:8]) #SLICLING FROM START
+
+
+y= "welcome back!"
+print(y[4: ]) #SLICING FROM START TO END
+
+
+z="hello everyone"
+print(z[ :9])  #SLICING FROM END TO START
+
+a="good morning"
+print(a[-8:-4])
+print(a[-7: ])
+print(a[ :-3]) #NEGATIVE SLICING
+
+
+
